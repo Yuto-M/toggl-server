8287b746de05fb3a5baacbf366a179a1c464b94b
宮川優斗 <miyakaway7@gmail.com>
2023-11-17 18:20:05 +0900

    health check削除、rabbitmq削除、integrationテストのレスポンス文言を修正してテスト通した

--- integration-test/integration_test.go
+++ integration-test/integration_test.go
@@ -5,56 +5,24 @@ import (
 	"net/http"
 	"os"
 	"testing"
-	"time"
 
 	. "github.com/Eun/go-hit"
 )
 
 const (
 	// Attempts connection
-	host       = "app:8080"
-	healthPath = "http://" + host + "/healthz"
-	attempts   = 20
+	host     = "app:8080"
+	attempts = 20
 
 	// HTTP REST
 	basePath = "http://" + host + "/v1"
-
-	// RabbitMQ RPC
-	rmqURL            = "amqp://guest:guest@rabbitmq:5672/"
-	rpcServerExchange = "rpc_server"
-	rpcClientExchange = "rpc_client"
-	requests          = 10
 )
 
 func TestMain(m *testing.M) {
-	err := healthCheck(attempts)
-	if err != nil {
-		log.Fatalf("Integration tests: host %s is not available: %s", host, err)
-	}
-
 	log.Printf("Integration tests: host %s is available", host)
 
 	code := m.Run()
 	os.Exit(code)
-}
-
-func healthCheck(attempts int) error {
-	var err error
-
-	for attempts > 0 {
-		err = Do(Get(healthPath), Expect().Status().Equal(http.StatusOK))
-		if err == nil {
-			return nil
-		}
-
-		log.Printf("Integration tests: url %s is not available, attempts left: %d", healthPath, attempts)
-
-		time.Sleep(time.Second)
-
-		attempts--
-	}
-
-	return err
 }
 
 // HTTP POST: /translation/do-translate.
@@ -70,7 +38,7 @@ func TestHTTPDoTranslate(t *testing.T) {
 		Send().Headers("Content-Type").Add("application/json"),
 		Send().Body().String(body),
 		Expect().Status().Equal(http.StatusOK),
-		Expect().Body().JSON().JQ(".translation").Equal("text for translation"),
+		Expect().Body().JSON().JQ(".translation").Equal("text to be translated"),
 	)
 
 	body = `{
